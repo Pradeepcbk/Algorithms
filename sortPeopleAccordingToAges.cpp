0872678fe1818404988b45e590b28fcf4959f0fe
Pradeep <34934343+Pradeepcbk@users.noreply.github.com>
2020-03-20 12:56:46 +0100

    Update sortPeopleAccordingToAges.cpp

--- sortPeopleAccordingToAges.cpp
+++ sortPeopleAccordingToAges.cpp
@@ -1,14 +1,43 @@
+/**
+* @section License
+*
+* The MIT License (MIT)
+*
+* Copyright (c) 2017, Prajwal Nagaraja
+*
+* Permission is hereby granted, free of charge, to any person
+* obtaining a copy of this software and associated documentation
+* files (the "Software"), to deal in the Software without
+* restriction, including without limitation the rights to use, copy,
+* modify, merge, publish, distribute, sublicense, and/or sell copies
+* of the Software, and to permit persons to whom the Software is
+* furnished to do so, subject to the following conditions:
+*
+* The above copyright notice and this permission notice shall be
+* included in all copies or substantial portions of the Software.
+*
+* THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+* EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+* MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+* NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS
+* BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN
+* ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN
+* CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+* SOFTWARE.
+*
+*/
+
 #include "stdafx.h"
 #include <iostream>
 #include <string>
 #include <algorithm>
-#include <fstream>
+#include<fstream>
 #include <iostream>
 #include <sstream>
 #include <vector>
 #include <map>
 #include <functional>
-#include <string>
+#include<string>
 
 using namespace std;
 
@@ -20,6 +49,9 @@ class Person {
 		Person(string s, int n): name(s), age(n){}
 		int getAge() {
 			return age;
+		}
+		string getName() {
+			return name;
 		}
 };
 
@@ -40,6 +72,10 @@ int main() {
 		myVector.push_back(p);
 	}
 	sort(myVector.begin(), myVector.end(), compare);
+	cout << "The list of people in the decreasing order of their age is: \n";
+	for (auto personCounter : myVector) {
+		cout << personCounter.getName() + " " << personCounter.getAge() << endl;
+	}
 
 	return 0;
 }
