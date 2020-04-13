e2488f1f53b37901ef4250a66e87b11cae5185bb
Pradeep <34934343+Pradeepcbk@users.noreply.github.com>
2020-04-13 13:02:44 +0200

    Create findTheLikedGenreOfBooks.cpp

---
+++ findTheLikedGenreOfBooks.cpp
@@ -0,0 +1,94 @@
+/**
+* @section License
+*
+* The MIT License (MIT)
+*
+* Copyright (c) 2017, Pradeep Siddagangaiah
+*
+* Permission is hereby granted, free of charge, to any person
+* obtaining a copy of this software and associated documentation
+* files (the "Software"), to deal in the Software without
+* restriction, including without limitation the rights to use, copy,
+* modify, merge, publish, distribute, sublicense, and/or sell copies
+* of the Software, and to permit persons to whom the Software is
+* furnished to do so, subject to the following conditions:
+*
+* The above copyright notice and this permission notice shall be
+* included in all copies or substantial portions of the Software.
+*
+* THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+* EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+* MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+* NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS
+* BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN
+* ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN
+* CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+* SOFTWARE.
+*
+*/
+
+#include "stdafx.h"
+#include <iostream>
+#include <map>
+#include <vector>
+#include <string>
+#include <fstream>
+#include <algorithm>
+#include <functional>
+#include <stack>
+#include <unordered_map>
+
+using namespace std;
+
+class Genre2books {
+public:
+	string genre;
+	vector<string> books;
+	Genre2books(string s, vector<string> sv) :genre(s), books(sv) {}
+};
+
+int main() {
+	map<string, vector<string>> name2Books;
+	map<int, string> countMap = { {0,"Intellectual"},{1,"Personality Development"},{2,"Suspense Thriller"},{3,"Family Drama"},{4,"Science Fiction"} };
+	map<int,int> counter;
+	vector<Genre2books> genre2BooksVector;
+	name2Books.insert({ "Gowtham",{ "A girl on the train", "What got you here won't get you there" } });
+	name2Books.insert({ "Pradeep",{ "Yana", "Martian", "Jugaari Cross" } });
+	name2Books.insert({ "Sandeep",{ "Mookajjiya Kanasugalu", "Jugaari Cross", "A Murder is announced" } });
+	name2Books.insert({ "Suhas",{ "A girl on the train", "Yana" } });
+	Genre2books intellectual("Intellectual", { "Mookajjiya Kanasugalu" });
+	Genre2books pd("Personality Development", { "What got you here won't get you there" });
+	Genre2books suspense("Suspense Thriller", { "Jugaari Cross", "A Murder is announced" });
+	Genre2books drama("Family Drama", { "A girl on the train" });
+	Genre2books scifi({ "Science Fiction",{ "Yana", "Martian" } });
+	genre2BooksVector.push_back(intellectual);
+	genre2BooksVector.push_back(pd);
+	genre2BooksVector.push_back(suspense);
+	genre2BooksVector.push_back(drama);
+	genre2BooksVector.push_back(scifi);
+	for (auto it = name2Books.begin();it != name2Books.end();it++) {
+		vector<string> booksName = it->second;
+		counter.clear();
+		for (vector<string>::iterator b = booksName.begin();b != booksName.end();b++) {
+			for (int i = 0;i < genre2BooksVector.size();i++) {
+				auto result = find(genre2BooksVector[i].books.begin(), genre2BooksVector[i].books.end(), *b);
+				if (result != genre2BooksVector[i].books.end()) {
+					counter[i]++;
+				}
+			}
+		}
+		auto genre = max_element(counter.begin(), counter.end());
+		int temp = 0;
+		for (int i = 0;i < counter.size();i++) {
+			if (genre->second == counter[i]) {
+				if (temp == 0) {
+					cout << it->first << " likes books of genre " << countMap[i];
+					temp = 1;
+				}
+				else cout << " & " << countMap[i];
+			}
+			if(i == counter.size()-1) cout << "\n";
+		}
+	}
+	return 0;
+}
